61537ce84221d1087f580d20a50f8194bced8789
alexcareyfb456-ui <alexcareyfb456@gmail.com>
2026-05-05 20:16:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://video-clip-frontend.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
